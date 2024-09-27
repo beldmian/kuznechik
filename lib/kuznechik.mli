c76ce8c87711bea0c304b58118eb473c54294f34
Dmitry Belousov <me@beldmian.ru>
2024-09-28 01:00:47 +0300

    Improve performance metrics + add benchmark

--- lib/kuznechik.mli
+++ lib/kuznechik.mli
@@ -1,12 +1,18 @@
-val generate_key : bytes
-(** [generate_key] generates random 256-bit key to be used in encryption and decription*)
+module Cipher : sig
+  type t = {key: int array; ik: int array array}
 
-val encrypt_block :
-  bytes (** 256-bit key value *) -> bytes (** 128-bit block value *) -> bytes
-(** [encrypt_block key block] encrypts one [block] of 128-bit using provided [key]*)
+  val make : int array -> t
+  (** [make key] creates instance of [Cipher.t] using provided [key]*)
 
-val decrypt_block :
-     bytes (** 256-bit key value *)
-  -> bytes (** 128-bit encrypted block value *)
-  -> bytes
-(** [decrypt_block key enc_block] decrypts one encrypted block [enc_block] of 128-bit using provided [key]*)
+  val encrypt_block :
+       t (** cipher instance *)
+    -> int array (** 128-bit block value *)
+    -> int array
+  (** [encrypt_block cipher block] encrypts one [block] of 128-bit using provided [cipher]*)
+
+  val decrypt_block :
+       t (** cipher instance *)
+    -> int array (** 128-bit encrypted block value *)
+    -> int array
+  (** [decrypt_block cipher enc_block] decrypts one encrypted block [enc_block] of 128-bit using provided [cipher]*)
+end
